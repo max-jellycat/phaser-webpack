fa0fbf086261709456ba89caaddd442c9d9a7da9
max-jellycat <max@jellycat.fr>
2020-02-13 00:20:09 +0100

    Enemies group + collision

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -11,7 +11,8 @@ class Play extends Phaser.Scene {
     this.tileSize = 32;
     this.playerSpeed = 2;
     this.enemyMinSpeed = 2;
-    this.enemyMaxSpeed = 5;
+    this.enemyMaxSpeed = 4;
+    this.numberOfEnemies = 5;
 
     // boundaries
     this.enemyMinY = 0;
@@ -33,15 +34,29 @@ class Play extends Phaser.Scene {
       .sprite(32, this.gameHeight / 2 - this.tileSize, 'player')
       .setOrigin(0);
 
-    this.enemy = this.add
-      .sprite(128, this.gameHeight / 2 - this.tileSize, 'dragon')
-      .setOrigin(0);
+    this.enemies = this.add.group({
+      key: 'dragon',
+      repeat: this.numberOfEnemies - 1,
+      setXY: {
+        x: 128,
+        y: 32,
+        stepX: 96,
+        stepY: 32
+      }
+    });
 
-    const dir = Math.random() < 0.5 ? 1 : -1;
-    const randomSpeed =
-      this.enemyMinSpeed +
-      Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
-    this.enemy.speed = dir * randomSpeed;
+    Phaser.Actions.Call(
+      this.enemies.getChildren(),
+      function(enemy) {
+        enemy.setOrigin(0);
+        const dir = Math.random() < 0.5 ? 1 : -1;
+        const randomSpeed =
+          this.enemyMinSpeed +
+          Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
+        enemy.velocity = dir * randomSpeed;
+      },
+      this
+    );
 
     this.goal = this.add
       .sprite(
@@ -49,10 +64,8 @@ class Play extends Phaser.Scene {
         this.gameHeight / 2 - this.tileSize,
         'chest_closed'
       )
-      .setOrigin(0);
+      .setOrigin(0.5, 0);
   }
-
-  createText() {}
 
   update(dt) {
     if (this.input.activePointer.isDown) {
@@ -64,16 +77,33 @@ class Play extends Phaser.Scene {
     const goalRect = this.goal.getBounds();
 
     if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, goalRect)) {
-      this.scene.manager.bootScene(this);
+      this.scene.restart();
     }
 
+    // Get enemies
+    const enemies = this.enemies.getChildren();
     // enemy movement
-    this.enemy.y += this.enemy.speed;
 
-    if (this.enemy.speed < 0 && this.enemy.y <= this.enemyMinY)
-      this.enemy.speed *= -1;
-    if (this.enemy.speed > 0 && this.enemy.y >= this.enemyMaxY)
-      this.enemy.speed *= -1;
+    Phaser.Actions.Call(
+      this.enemies.getChildren(),
+      function(enemy) {
+        enemy.y += enemy.velocity;
+
+        const conditionMin = enemy.velocity < 0 && enemy.y <= this.enemyMinY;
+        const conditionMax = enemy.velocity > 0 && enemy.y >= this.enemyMaxY;
+
+        if (conditionMin || conditionMax) enemy.velocity *= -1;
+
+        const enemyRect = enemy.getBounds();
+
+        if (
+          Phaser.Geom.Intersects.RectangleToRectangle(playerRect, enemyRect)
+        ) {
+          this.scene.restart();
+        }
+      },
+      this
+    );
   }
 
   render() {}
